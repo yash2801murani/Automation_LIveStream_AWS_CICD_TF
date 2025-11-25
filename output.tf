bc7197fbaab534e2c10b405ee53c5b3ea0b43b72
Yash Murani <yashmurani@Yashs-MacBook-Air.local>
2025-11-25 15:52:26 +0530

    final changes

--- output.tf
+++ output.tf
@@ -1,5 +1,30 @@
-output "ivs_channel_arn" {
-  value = aws_ivs_channel.live.arn
+# output "ivs_channel_arn" {
+#   value = aws_ivs_channel.live.arn
+# }
+
+# output "ivs_playback_url" {
+#   value = aws_ivs_channel.live.playback_url
+# }
+
+# # output "ivs_stream_key" {
+# #   value     = data.aws_ivs_stream_key.stream_key.value
+# #   sensitive = true
+# # }
+
+# output "ivs_stream_key" {
+#   value     = data.aws_ivs_stream_key.stream_key.value
+#   sensitive = false
+# }
+
+# output "s3_index_url" {
+#   value = "http://${aws_s3_bucket.site.bucket}.s3.amazonaws.com/index.html"
+# }
+
+
+
+
+output "ivs_ingest_url" {
+  value = aws_ivs_channel.live.ingest_endpoint
 }
 
 output "ivs_playback_url" {
@@ -8,10 +33,9 @@ output "ivs_playback_url" {
 
 output "ivs_stream_key" {
   value     = data.aws_ivs_stream_key.stream_key.value
-  sensitive = true
+  sensitive = false   # Make visible in Jenkins console
 }
 
 output "s3_index_url" {
   value = "http://${aws_s3_bucket.site.bucket}.s3.amazonaws.com/index.html"
 }
-
